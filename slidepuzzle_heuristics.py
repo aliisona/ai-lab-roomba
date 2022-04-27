8e9884781f20b0fccb6fccc6bd030d6d90fe79d6
Vedic Patel <71410506+RobotKing888@users.noreply.github.com>
2022-04-26 20:46:10 -0400

    Update slidepuzzle_heuristics.py

--- slidepuzzle_heuristics.py
+++ slidepuzzle_heuristics.py
@@ -1,5 +1,5 @@
 # Lab 1, Part 2a: Heuristics.
-# Name(s): 
+# Name(s): Alisona Le, Vedic Patel
 from search_heuristics import *
 from slidepuzzle_problem import *
 
@@ -11,11 +11,32 @@ INF = float('inf')
 
 """ Return the Hamming distance (number of tiles out of place) of the SlidePuzzleState """
 def slidepuzzle_hamming(state : SlidePuzzleState)  -> float:
-    raise NotImplementedError
-
+    num=0
+    ham=0
+    for row in state.tiles:
+        for col in row:
+            if(num!=0):
+                if(col!=num):
+                    ham+=1
+            num+=1
+    return ham
 """ Return the sum of Manhattan distances between tiles and goal of the SlidePuzzleState """
 def slidepuzzle_manhattan(state : SlidePuzzleState)  -> float:
-    raise NotImplementedError
+    num=0
+    man=0
+    
+    for X in range(len(state.tiles)):
+        row = state.tiles[X]
+
+        for Y in range (len(row)):
+            col = row[Y]
+            if(num!=0):
+                if (num != col):
+                    goalX= (int)(col % len(row))
+                    goalY= (int)(col / len(row))
+                    man += abs(goalX - X) + abs(goalY - Y)
+            num += 1
+    return man   
 
 SLIDEPUZZLE_HEURISTICS = {
     "Zero" : zero_heuristic, 
